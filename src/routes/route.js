acc1b930782886289476fc61cf3bdb583f0863b9
shariqrahman <shariq.rahman.india@gamil.com>
2022-02-25 03:13:53 +0530

    Query-Param

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,131 +1,66 @@
 const express = require('express');
-const router = express.Router();
-const players = [ {
-    name: "manish",
-    dob: "1/1/1995",
-    gender: "male",
-    city: "jalandhar",
-    sports: ["swimming"],
-    bookings: [
-                {
-                bookingNumber: 1,
-                sportId: "",
-                centerId: "",
-                type: "private",
-                slot: 16286598000000,
-                bookedOn: 31/08/2021,
-                bookedFor: 01/09/2021
-                },
-                {
-                    bookingNumber: 2,
-                    sportId: "",
-                    centerId: "",
-                    type: "private",
-                    slot: 162865980000880,
-                    bookedOn: 31/08/2022,
-                    bookedFor: 01/11/2021
-                    }
-    
-                ]}, 
-    {
-        name: "parth",
-        dob: "1/1/1993",
-        gender: "male",
-        city: "amreli",
-        sports: ["swimming"],
-        bookings:[]
-    },
-    {
-        name: "suraj",
-        dob: "1/1/1992",
-        gender: "male",
-        city: "gwalior",
-        sports: ["football"],
-        bookings:[]
-    },
-    {
-        name: "prafful",
-        dob: "1/1/1991",
-        gender: "male",
-        city: "bharuch",
-        sports: ["running"],
-        bookings:[]
-    },
-    {
-        name: "indra",
-        dob: "1/1/1991",
-        gender: "male",
-        city: "bharuch",
-        sports: ["cash"],
-        bookings:[
-            {
-                bookingNumber: 5,
-                sportId: "",
-                centerId: "",
-                type: "private",
-                slot: 162865980001880,
-                bookedOn: 31/08/2020,
-                bookedFor: 01/11/2020
-                }
+const router = express.Router()
 
-        ]
-    }
+
+// ASSIGNMENT:
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+// WRITE A POST API TO THE ABOVE
+
+// take this as sample for array of persons:
+
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
 ]
-    router.post('/player/:name/:dob/:gender/:city/:sports/:bookings', function(req, res){
-        let userName = req.params.name
-        let userDob = req.params.dob
-        let userGender = req.params.gender
-        let userCity = req.params.city
-        let userSports = req.params.sports
-        let userBooking = req.params.bookings
 
-        let newPlayer = {}
-        newPlayer.name = userName
-        newPlayer.dob = userDob
-        newPlayer.gender =userGender
-        newPlayer.city = userCity
-        newPlayer.sports = [userSports]
-        newPlayer.bookings = [userBooking]
-        for (let i =0; i<players.length; i++){
-            if(players[i].name != userName){
-                players.push(newPlayer)
-                console.log(newPlayer)
-                res.send(players)
-            }else{
-                res.send("name already exist")
-                break;
-            }
-        }
-        
+
+router.post("/election", function (req, res) {
+   let votingAge = req.query.votingAge
+
+   let voterList = [];
+   for (let i = 0; i < persons.length; i++) {
+
+       if (persons[i].age > votingAge) {
        
-    })
-    
-    router.post('/players/:playerName/bookings/:bookingId', function(req, res){
-        let userName = req.params.playerName
-        let userBookingId = req.params.bookingId
-        let userBookings = req.body
-        
-        for(let i=0; i<players.length; i++){
-            if (players[i].name == userName){
-                let arr = players[i].bookings
-                if(arr.length === 0){
-                    arr.push(userBookings)
-                    res.send(players)
-                }else{
-                for (let j=0; j<arr.length; j++){
-                    if(arr[j].bookingNumber != userBookingId) {
-                        arr.push(userBookings)
-                        res.send(players)
-                    }else{
-                        res.send("booking ID already exist")
-                    }
-                }
-            }
-            
-        } 
-            
-    }
-    res.send("name does not exist")
+           persons[i].votingStatus = true
+           voterList.push(persons[i])
+       }
+   }
+if (voterList.length>0)
+{
+   return res.send(voterList)
+}
+else{
+   return res.send("no member found above this age")
+}
+
 })
- 
+
+
 module.exports = router;
