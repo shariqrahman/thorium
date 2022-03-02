ac21fe841a55373bdbe61d3095c6d3d5c9cc16bd
shariqrahman <shariq.rahman.india@gamil.com>
2022-03-03 02:05:07 +0530

    Populate and Reference

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,20 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
+const bookController= require("../controllers/newBookController")
+const publisherController = require("../controllers/publisherController")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", authorController.createAuthor  )
+router.post("/createAuthor", authorController.createAuthor)
 
 router.get("/getAuthorsData", authorController.getAuthorsData)
 
-router.post("/createBook", bookController.createBook  )
+router.post("/createPublisher", publisherController.createPublisher)
+
+router.get("/getPublisherData", publisherController.getPublisherData)
+
+router.post("/createBook", bookController.createBook)
 
 router.get("/getBooksData", bookController.getBooksData)
 
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
 
 module.exports = router;
